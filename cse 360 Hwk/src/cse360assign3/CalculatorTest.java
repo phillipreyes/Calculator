ac9674270346201c2ca823f0e1904abf4c9e1ccc
Phillip S. Reyes <psreyes@asu.edu>
2016-02-21 16:58:20 -0700

    Initial checkin

---
+++ cse 360 Hwk/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,66 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testCalculator(){
+		Calculator test= new Calculator();
+		assertNotNull(test);
+	}
+	
+	@Test
+	public void testGetTotal () {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testAdd (int value) {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testSubtract (int value) {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMultiply (int value) {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDivide (int value) {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testgetHistory () {
+		fail("Not yet implemented");
+	}
+}
