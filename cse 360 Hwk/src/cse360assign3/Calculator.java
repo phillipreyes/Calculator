e6bb8689d52895bcb6530241e08944e4b1555581
Phillip S. Reyes <phil.s.reyes@gmail.com>
2016-02-21 18:37:44 -0700

    Create second version changes the methods respectively to add, subtract, multiply and divide to total. Updated CalculatorTest class to test updated methods in Calculator class

--- cse 360 Hwk/src/cse360assign3/Calculator.java
+++ cse 360 Hwk/src/cse360assign3/Calculator.java
@@ -1,5 +1,11 @@
 
 package cse360assign3;
+/**
+ * Program will do basic math arithmatic 
+ * Also allow program to commit in github
+ * @author Philllip Reyes
+ *
+ */
 
 public class Calculator {
 
@@ -8,25 +14,49 @@ public class Calculator {
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
-	
+	/**
+	 * This is our getter for the total
+	 * @return
+	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
+	/**
+	 * this will add an integer to value
+	 * @param value
+	 */
 	
 	public void add (int value) {
-		
+		total=total+value;
 	}
 	
+	/**
+	 * this will subtract from value
+	 * @param value
+	 */
 	public void subtract (int value) {
-		
+		total=total-value;
 	}
-	
+	/**
+	 * this will multiply integer to value
+	 * @param value
+	 */
 	public void multiply (int value) {
+		total=total*value;
 		
 	}
-	
+	/**
+	 * this will divide integer to value and will return zero is value is equal to zero
+	 * @param value
+	 */
 	public void divide (int value) {
 		
+		if(value == 0){
+			total=0;
+		}else{
+		 
+			total=total/value;
+		}
 	}
 	
 	public String getHistory () {
--- cse 360 Hwk/src/cse360assign3/CalculatorTest.java
+++ cse 360 Hwk/src/cse360assign3/CalculatorTest.java
@@ -13,54 +13,85 @@ public class CalculatorTest {
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
-
+	
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+	
 	}
 
 	@Before
 	public void setUp() throws Exception {
+	
 	}
 
 	@After
 	public void tearDown() throws Exception {
+	
 	}
 
 	@Test
 	public void test() {
+		
 		fail("Not yet implemented");
 	}
 	@Test
 	public void testCalculator(){
+		
 		Calculator test= new Calculator();
 		assertNotNull(test);
 	}
 	
 	@Test
 	public void testGetTotal () {
-		fail("Not yet implemented");
+		
+		Calculator test= new Calculator();
+		assertEquals(5,test.getTotal());
+	
 	}
 	@Test
-	public void testAdd (int value) {
-		fail("Not yet implemented");
+	public void testAdd () {
+		
+		Calculator test= new Calculator();
+		test.add(5);
+		assertEquals(0,test.getTotal());
+		
 	}
 	@Test
-	public void testSubtract (int value) {
-		fail("Not yet implemented");
+	public void testSubtract () {
+		
+		Calculator test= new Calculator();
+		test.subtract(8);
+		assertEquals(-8,test.getTotal());
 	}
 	
 	@Test
-	public void testMultiply (int value) {
-		fail("Not yet implemented");
+	public void testMultiply () {
+		
+		Calculator test= new Calculator();
+		test.multiply(5);
+		assertEquals(0,test.getTotal());
 	}
 	
 	@Test
-	public void testDivide (int value) {
-		fail("Not yet implemented");
+	public void testDivide () {
+		
+		Calculator test= new Calculator();
+		test.divide(9);
+		assertEquals(0,test.getTotal());
 	}
-	
+	@Test
+	public void testDivideByZero(){
+		
+		Calculator test= new Calculator();
+		test.add(5);
+		test.divide(0);
+		assertEquals(0,test.getTotal());
+	}
 	@Test
 	public void testgetHistory () {
+		
+		Calculator test= new Calculator();
 		fail("Not yet implemented");
+	
 	}
 }
